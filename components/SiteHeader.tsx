976262dc4306906f9a60c4def44bc2a5e06da73b
kosbushe <k.byshevoy@gmail.com>
2026-07-11 16:05:44 +0300

    feat: add school literature navigator homepage

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -5,8 +5,8 @@ export function SiteHeader() {
     <header className="site-header">
       <Link className="wordmark" href="/">ЛИТЕРАТУРА.ГАЙД</Link>
       <nav aria-label="Основная навигация">
-        <Link href="/authors/leo-tolstoy">Толстой</Link>
-        <Link href="/works/after-the-ball">После бала</Link>
+        <Link href="/#program">Программа</Link>
+        <Link href="/works/after-the-ball">Первый маршрут</Link>
         <Link href="/sources">Источники</Link>
       </nav>
     </header>
